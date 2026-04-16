eeba9d6783b2abd9e96afe5b50c304a71939cafe
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-04-16 11:29:41 +0200

    remove incorrect comment due to recent changes

--- src/main/java/evaluation/Environment.java
+++ src/main/java/evaluation/Environment.java
@@ -161,8 +161,6 @@ public class Environment {
      * This method takes a query and modifies it such that it executes inside the environment. It adds a context-item
      * declaration, variable declarations and replaces URIs with the right filepaths.
      *
-     * It intentionally does NOT prepend namespace or decimal-format declarations.
-     * Use prependProlog() for that.
      *
      * @param query contains the query that wants to be executed.
      * @return a String containing the updated query with the context-item, params and resources set.
